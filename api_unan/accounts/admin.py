7ebabda25a7a227f05f4c3544ad584a314d37deb
ferjovel06 <fermajobe@gmail.com>
2025-02-19 19:35:00 -0600

    Update admin.py

--- api_unan/accounts/admin.py
+++ api_unan/accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Account, Student, Teacher
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_admin', 'is_active', 'is_superuser')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    list_filter = ('is_admin', 'is_active', 'is_superuser')
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('account', 'career', 'year_in_course', 'student_id')
+    search_fields = ('account', 'career', 'year_in_course', 'student_id')
+    list_filter = ('career', 'year_in_course')
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('account', 'career', 'teacher_id')
+    search_fields = ('account', 'career', 'teacher_id')
+    list_filter = ('career',)
